38d2ab8aac37c0c48dcf4034e38b33cc14f22844
eric <543510981@qq.com>
2025-04-19 10:00:44 +0800

    project init: 引入依赖包和配置application

---
+++ src/main/java/com/eric/ericlike/EricLikeApplication.java
@@ -0,0 +1,13 @@
+package com.eric.ericlike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricLikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EricLikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eric/ericlike/EricLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.ericlike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricLikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
